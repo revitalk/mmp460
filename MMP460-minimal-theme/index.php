86249a8b89dd809fb74664cad43485d3f6515fc9
revitalk <revital.kaisar@gmail.com>
2018-02-20 12:41:07 -0500

    Update index.php

--- MMP460-minimal-theme/index.php
+++ MMP460-minimal-theme/index.php
@@ -1,31 +1,20 @@
 <!-- Page header -->           
 
 <?php get_header();?>
-
 	<div id="content">
     
 <!-- Starting posts-->
   
   <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-  
   <article class="blogPost">
-  
   <!-- Title of the post -->
-  
     <h2><a href="<?php the_permalink(); ?>"> 
       <?php the_title(); ?> 
       </a></h2>
-    
-    
     <!-- Post's content -->
-    
 	<?php the_content(); ?> 
-
     </article>
-    
-    
   <?php endwhile; else: ?>  
-  
   <p>
     <?php _e('Sorry, no posts matched your criteria.'); ?>
   </p>
